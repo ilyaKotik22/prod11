5c165eb18cbd24ea798537b1797abe0ee4b10216
ilyakotik22 <ilyakotik2204@gmail.com>
2025-12-07 16:15:00 +0300

    updata-mainPage

--- frontend/src/widgets/Home/homeBlock4/homeBlock4.tsx
+++ frontend/src/widgets/Home/homeBlock4/homeBlock4.tsx
@@ -5,8 +5,12 @@ import fon2 from '../../../../public/krasivye-i-sovremennye-rastenia-deko.jpg';
 import fon3 from '../../../../public/3d-sovremennyi-inter-er-gostinoi-i-sovremennaa-mebel.jpg';
 import fon4 from '../../../../public/Primavera.png';
 import fon5 from '../../../../public/Shift.png';
+import { useNavigate } from 'react-router-dom';
 
 export const HomeBlock4: React.FC = () => {
+
+  const navigate = useNavigate()
+
   return (
     <section className={styles.block4}>
       {/* Заголовок секции */}
@@ -25,11 +29,11 @@ export const HomeBlock4: React.FC = () => {
           {/* 1. Большой комплекс — Republic */}
           <div className={styles.featuredComplex}>
             <div className={styles.complexInfo}>
-              <span className={styles.tag}>Жилой кластер</span>
-              <h2><b>Бадаевские Пруды</b></h2>
-              <p className={styles.price}><b>Квартиры</b> от 14 900 000 ₽ от 42 м²</p>
-              <p className={styles.desc}>Квартал в историческом центре с видами на Кремль и собственным парком.</p>
-              <button className={styles.btn}>На страницу комплекса</button>
+              <span className={styles.tag}>Жилой комплекс</span>
+              <h2><b>ЖК Адали</b></h2>
+              <p className={styles.price}><b>Квартиры</b> от 5 900 000 ₽ от 42 м²</p>
+              <p className={styles.desc}>Современный жилой комплекс бизнес-класса расположен в сердце Советского района, на улице Аделя Кутуя, которая превращается в оживленный городской хаб с бизнес-средой и развитой инфраструктурой.</p>
+              <button onClick={()=> navigate('/new-building-complexes/13')} className={styles.btn}>На страницу комплекса</button>
             </div>
           </div>
 
@@ -39,7 +43,7 @@ export const HomeBlock4: React.FC = () => {
             <div className={styles.rentalInfo}>
               <div className={styles.rentalTags}>
                 <b>Квартира в аренду</b>
-                <span>Номер лота: 4216</span>
+                <span>Номер лота: 701</span>
               </div>
               <h1>
                 Уютная<br />
@@ -48,7 +52,7 @@ export const HomeBlock4: React.FC = () => {
                 под ключ
               </h1>
               <p className={styles.price}><b>Студия — 85 000 ₽/месяц</b></p>
-              <button className={styles.btn}>На страницу лота</button>
+              <button onClick={()=> navigate('/rental-apartments/38')} className={styles.btn}>На страницу лота</button>
             </div>
           </div>
 
@@ -77,12 +81,12 @@ export const HomeBlock4: React.FC = () => {
             <img src={fon4} alt="Shift" className={styles.shiftMainImg} />
             <div className={styles.shiftOverlay}>
               
-              <div className={styles.shiftInfo}>
+              <div  className={styles.shiftInfo}>
                 <span className={styles.tag}>Жилой комплекс</span>
-                <h1><b>Level Нагатино</b></h1>
-                <p className={styles.price}>Квартиры от 17 200 000 ₽ от 48 м²</p>
-                <p className={styles.desc}>Современный район на юге Москвы с собственной инфраструктурой и экопарком.</p>
-                <button className={styles.btn}>На страницу комплекса</button>
+                <h1><b>ЖК Яналиф</b></h1>
+                <p className={styles.price}>Квартиры от 5 200 000 ₽ от 48 м²</p>
+                <p className={styles.desc}>Жилой комплекс — это проект, объединяющий искусство и технологичность в многофункциональное пространство. Изысканная архитектура, премиальное лобби, консьерж-сервис и все возможности инфраструктуры центра столицы Татарстана</p>
+                <button onClick={()=> navigate('/new-building-complexes/14')} className={styles.btn}>На страницу комплекса</button>
               </div>
             </div>
           </div>
